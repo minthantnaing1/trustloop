34fa0420e33d56c8ac206e60550a16956cd1b204
Min Thant Naing <u6530233@au.edu>
2025-06-26 15:21:35 +0700

    Update Ui with Tailwind & Add Filter, Delete

---
+++ app/test/page.js
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-white">
+      <button className="bg-[#325082] text-white px-6 py-3 rounded-md hover:bg-white hover:text-[#325082] border hover:border-[#325082] transition">
+        Hover Me
+      </button>
+    </div>
+  );
+}
--- components/LoginButton.js
+++ components/LoginButton.js
@@ -3,7 +3,7 @@
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 
-function LoginButton() {
+export default function LoginButton() {
   const [isLoading, setIsLoading] = useState(false);
 
   const handleLogin = async () => {
@@ -15,17 +15,15 @@ function LoginButton() {
   return (
     <button
       onClick={handleLogin}
-      className="microsoft-btn"
       disabled={isLoading}
+      className="flex items-center justify-center gap-2 border border-gray-300 bg-white px-4 py-3 rounded-md text-[14px] font-medium w-full hover:bg-gray-100 transition"
     >
       <img
         src="https://purepng.com/public/uploads/large/purepng.com-microsoft-logo-iconlogobrand-logoiconslogos-251519939091wmudn.png"
         alt="Microsoft logo"
-        className="microsoft-icon"
+        className="h-[20px] w-[20px]"
       />
       {isLoading ? "Signing in..." : "Login with Microsoft"}
     </button>
   );
 }
-
-export default LoginButton;
